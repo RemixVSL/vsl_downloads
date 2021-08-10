f2e4141b6a55e9ad448177525bb559c773f1fd3a
Bill Plein <260078+bplein@users.noreply.github.com>
2021-08-10 13:58:44 -0500

    fix tar flags

--- mk_vsl4_firmware.sh
+++ mk_vsl4_firmware.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 echo "Creating fusion_4.3.7-20200113.fff from the split files"
-cat vsl4/fusion_4.3.7-20200113.fff.tgz.parta* | tar -xzv - fusion_4.3.7-20200113.fff
+cat vsl4/fusion_4.3.7-20200113.fff.tgz.parta* | tar -xjv - fusion_4.3.7-20200113.fff
 echo "Done! Looks for fusion_4.3.7-20200113.fff in your current directory"
