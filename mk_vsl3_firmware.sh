38dfc9c9124f18d4c70af502647aa133dba04893
Bill Plein <260078+bplein@users.noreply.github.com>
2021-08-10 13:56:14 -0500

    change folder structure

--- mk_vsl3_firmware.sh
+++ mk_vsl3_firmware.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 echo "Creating fusion_3.2.16-20180821.fff from the split files"
-cat Fusion-io\ VSL\ 3.x/fusion_3.2.16-20180821.fff.tgz.parta* | tar -xzv - fusion_3.2.16-20180821.fff
+cat vls3/fusion_3.2.16-20180821.fff.tgz.parta* | tar -xzv - fusion_3.2.16-20180821.fff
 echo "Done! Looks for fusion_3.2.16-20180821.fff in your current directory"
--- mk_vsl4_firmware.sh
+++ mk_vsl4_firmware.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 echo "Creating fusion_4.3.7-20200113.fff from the split files"
-cat Fusion-io\ VSL\ 4.x/fusion_4.3.7-20200113.fff.tgz.parta* | tar -xzv - fusion_4.3.7-20200113.fff
+cat vsl4/fusion_4.3.7-20200113.fff.tgz.parta* | tar -xzv - fusion_4.3.7-20200113.fff
 echo "Done! Looks for fusion_4.3.7-20200113.fff in your current directory"
