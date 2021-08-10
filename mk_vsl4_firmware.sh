8b84c47072c40b1736d83084abdd0b2a5013a570
Bill Plein <260078+bplein@users.noreply.github.com>
2021-08-10 14:31:16 -0500

    Update mk_vsl4_firmware.sh

--- mk_vsl4_firmware.sh
+++ mk_vsl4_firmware.sh
@@ -3,6 +3,6 @@ echo "Creating fusion_4.3.7-20200113.fff from the split files"
 echo "--------------"
 files=$(find vsl4 | grep "fff.tgz.part" | sort)
 #echo $files
-cat $files | tar -xzv usion_4.3.7-20200113.fff
+cat $files | tar -xzv fusion_4.3.7-20200113.fff
 echo "--------------"
-echo "Done! Looks for usion_4.3.7-20200113.fff in your current directory"
+echo "Done! Looks for fusion_4.3.7-20200113.fff in your current directory"
